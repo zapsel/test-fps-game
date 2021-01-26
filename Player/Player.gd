f8ca22465556a6dc7529d2fc13bb092a6dbfac5c
Julian <julianjo2@gmail.com>
2021-01-26 00:40:42 -0800

    Added reset button
    press g to reset level

--- Player/Player.gd
+++ Player/Player.gd
@@ -27,7 +27,10 @@ func _input(event):
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-
+		
+	if Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
+		
 func _physics_process(delta):
 	var head_basis = head.get_global_transform().basis
 	
